1b9033c545b421beb86efbd4618b78a6940d2d35
Andrew Leonard <leonardishere@gmail.com>
2020-07-21 06:51:54 -0700

    remove error handling, let it all go to dead letter queue

--- api/process_upload.py
+++ api/process_upload.py
@@ -15,49 +15,16 @@ headers = {
 }
 
 def handler(event, context):
-    try:
-        print(event)
-        print(json.dumps(event, indent=2))
-        #s3.upload_fileobj(json.dumps(event, indent=2), PICTURE_BUCKET, 'test_event')
-        s3.put_object(Body=json.dumps(event, indent=2), Bucket=PICTURE_BUCKET, Key='test_event')
-    except Exception as e:
-        print(e)
-        print('Error putting object')
-        #raise e
-        return {
-            'statusCode': 500,
-            'headers': headers,
-            'body': str(e)
-        }
+    # Get the object from the event
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
 
-    # Get the object from the event and show its content type
-    try:
-        bucket = event['Records'][0]['s3']['bucket']['name']
-        key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-    except Exception as e:
-        print(e)
-        print('Error getting bucket and key from event')
-        #raise e
-        return {
-            'statusCode': 500,
-            'headers': headers,
-            'body': 'Error getting bucket and key from event'
-        }
-    try:
-        s3_object = s3.get_object(Bucket=bucket, Key=key)
-        new_key = 'output_{}'.format(key) # TODO: 'output_{}'.format(key.split('_'[1]))
-        s3.put_object(Body=s3_object, Bucket=PICTURE_BUCKET, Key='test_event')
-        return {
-            'statusCode': 200,
-            'headers': headers,
-            'body': json.dumps({'NewKey': new_key})
-        }
-    except Exception as e:
-        print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-        #raise e
-        return {
-            'statusCode': 500,
-            'headers': headers,
-            'body': 'Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket)
-        }
+    s3_object = s3.get_object(Bucket=bucket, Key=key)
+    new_key = 'output_{}'.format(key) # TODO: 'output_{}'.format(key.split('_'[1]))
+    #s3.upload_fileobj(json.dumps(event, indent=2), PICTURE_BUCKET, 'test_event')
+    s3.put_object(Body=s3_object, Bucket=PICTURE_BUCKET, Key='test_event')
+    return {
+        'statusCode': 200,
+        'headers': headers,
+        'body': json.dumps({'NewKey': new_key})
+    }
